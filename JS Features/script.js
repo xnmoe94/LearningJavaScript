d918e118a55b6d0e1141a258f5ee5170ba56bf42
Mohamed abdullahi <Muhammadpeteng@gmail.com>
2020-09-04 21:57:00 -0500

    Updated

--- JS Features/script.js
+++ JS Features/script.js
@@ -18,3 +18,57 @@ const Multiplication = (...nums) => (
     nums.reduce((total, currval) => total * currval)
 
 )
+
+
+
+const getStats = (arr) => {
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    const sum = arr.reduce((sum, r) => sum + r);
+    const avg = sum / arr.length;
+    // Using the new shorthand property syntax:
+    return {
+        max,
+        min,
+        sum,
+        avg
+    }
+}
+const reviews = [4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5];
+
+const stats = getStats(reviews);
+
+function pick(arr) {
+    //return random element from arr
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
+
+
+
+
+function getCard() {
+    const values = [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        'J',
+        'Q',
+        'K',
+        'A'
+    ];
+    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+    const value = pick(values);
+    const suit = pick(suits)
+    return {
+        value,
+        suit
+    };
+}
